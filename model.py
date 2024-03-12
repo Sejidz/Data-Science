eb0a10636f9011d79a7aa73f0d76cf0982d7ae33
s2526239 <s2526239@vuw.leidenuniv.nl>
2024-03-12 12:21:31 +0100

    sku id segment

--- model.py
+++ model.py
@@ -10,38 +10,35 @@ import numpy as np  # noqa
 # Bokeh libraries
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
-#from bokeh.models import ColumnDataSource  # noqa
-#from bokeh.layouts import row, column, gridplot  # noqa
-#from bokeh.models import TabPanel  # noqa
+from bokeh.io import output_notebook
+from bokeh.palettes import Category10
+from bokeh.models import ColumnDataSource
+df = pd.read_csv("combined_sales_last.csv")
 
+# Data preprocessing
+df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
+df['Day of Week'] = df['Transaction Date'].dt.day_name()
 
+# Segmentation by SKU ID
+# Remove non-numeric characters and convert 'Amount (Buyer Currency)' to numeric
 
+# Group by 'Sku Id' and sum 'Amount (Buyer Currency)' for each group
+sku_sales = df.groupby('Sku Id')['Amount (Merchant Currency)'].sum()
 
+print(sku_sales)
+# Visualization
+output_file("sales_volume_by_sku_id.html")
 
+p = figure(x_range=sku_sales.index.tolist(), height=350, title="Sales Volume by SKU ID",
+           toolbar_location=None, tools="")
 
+p.vbar(x=sku_sales.index.tolist(), top=sku_sales.values, width=0.9, line_color="white")
 
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+p.xaxis.major_label_orientation = "vertical"
+p.xaxis.axis_label = "SKU ID"
+p.yaxis.axis_label = "Sales Volume (In Euro's)"
 
-x = [1, 2, 1]
-y = [1, 1, 2]
-
-# Determine where the visualization will be rendered
-output_file("first_glyphs.html", title="First Glyphs")
-
-# Set up the figure(s)
-fig = figure(
-    title="My Coordinates",
-    height=300,
-    width=300,
-    x_range=(0, 3),
-    y_range=(0, 3),
-    toolbar_location=None,
-)
-
-# Connect to and draw the data
-fig.circle(x=x, y=y, color="green", size=10, alpha=0.5)
-
-# Organize the layout
-
-# Preview and save
-#show(fig)  # See what I made, and save if I like it
+show(p)
 
